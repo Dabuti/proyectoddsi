89a206fd978e54990966d9fd38cdbdcddb5c1ff8
i5 <imdabuti@gmail.com>
2014-01-26 15:31:16 +0100

    local

--- datos.sql
+++ datos.sql
@@ -13,6 +13,8 @@ insert into producto(nombre, descripcion, calorias) values('macarrones','1kb','3
 insert into producto(nombre, descripcion, calorias) values('queso','100gr','100');
 insert into producto(nombre, descripcion, calorias) values('huevo','1unidad','25');
 insert into producto(nombre, descripcion, calorias) values('tomate','tomate frito Solis','40');
+insert into producto(nombre, descripcion, calorias) values('aceite','0.7L aceite de oliva','30');
+insert into producto(nombre, descripcion, calorias) values('patatas','malla de 1kg','120');
 
 -- Insertar Supermercados
 insert into super(
@@ -28,7 +30,7 @@ insert into super(
                   'coviran', 'C/Paloma, 4', '958591961', 'www.coviran.es'
                   );
 
--- Insertar asociaciones
+-- Insertar asociaciones ProductoSuper
 insert into productosuper(idproducto, idsuper) values('2','1');
 insert into productosuper(idproducto, idsuper) values('2','2');
 insert into productosuper(idproducto, idsuper) values('3','1');
@@ -43,3 +45,107 @@ insert into productosuper(idproducto, idsuper) values('7','1');
 insert into productosuper(idproducto, idsuper) values('7','2');
 insert into productosuper(idproducto, idsuper) values('8','1');
 insert into productosuper(idproducto, idsuper) values('8','2');
+insert into productosuper(idproducto, idsuper) values('9','1');
+insert into productosuper(idproducto, idsuper) values('9','2');
+
+-- Insertar Dietas
+insert into dieta(
+                  nombre, descripcion, idusuario
+                  )
+            VALUES(
+                   'Mediterranea',
+                   'dieta equilibrada y variada',
+                   '2'
+                   );
+insert into dieta(
+                  nombre, descripcion, idusuario
+                  )
+            VALUES(
+                   'Atkins',
+                   'Reduccion de 6kg en 2 semanas',
+                   '3'
+                   );
+
+-- Asociación Dieta-Usuario
+insert into dietausuario(iddieta, idusuario) values('1', '2');
+insert into dietausuario(iddieta, idusuario) values('2', '3');
+
+-- Insertar Recetas
+insert into receta(nombre, personas, tiempo, descripcion)
+            VALUES('Macarrones tomate','1','30', 'Macarrones con queso y tomate');
+insert into receta(nombre, personas, tiempo, descripcion)
+            VALUES(
+                   'Patatas con huevos',
+                   '1',
+                   '35',
+                   'Patatas fritas con huevos'
+                   );
+insert into receta(nombre, personas, tiempo, descripcion)
+            VALUES(
+                   'Pollo al chilindron',
+                   '1',
+                   '95',
+                   'Pollo al chilindron'
+                   );
+insert into receta(nombre, personas, tiempo, descripcion)
+            VALUES(
+                   'Arroz a la cubana',
+                   '1',
+                   '40',
+                   'Arroz con tomate huevo platano salchichas'
+                   );
+insert into receta(nombre, personas, tiempo, descripcion)
+            VALUES(
+                   'Menestra de verduras',
+                   '1',
+                   '35',
+                   'Muchas verduras'
+                   );
+
+-- Asociaciones Receta-Producto
+insert into recetaproducto(idreceta, idproducto, cantidad)
+            VALUES('1','4','1');
+insert into recetaproducto(idreceta, idproducto, cantidad)
+            VALUES('1','7','2');
+insert into recetaproducto(idreceta, idproducto, cantidad)
+            VALUES('2','8','1');
+insert into recetaproducto(idreceta, idproducto, cantidad)
+            VALUES('2','6','2');
+insert into recetaproducto(idreceta, idproducto, cantidad)
+            VALUES('2','9','1');
+insert into recetaproducto(idreceta, idproducto, cantidad)
+            VALUES('3','2','1');
+
+-- Asociaciones de Dieta-Receta
+insert into dietareceta(iddieta, idreceta) VALUES('1','1');
+insert into dietareceta(iddieta, idreceta) VALUES('1','3');
+insert into dietareceta(iddieta, idreceta) VALUES('1','4');
+insert into dietareceta(iddieta, idreceta) VALUES('2','2');
+insert into dietareceta(iddieta, idreceta) VALUES('2','5');
+
+-- Compras usuarios
+insert into compra(idusuario) VALUES(2);
+insert into compra(idusuario) VALUES(3);
+
+-- Linea de compra
+insert into compraproductosuper(idcompra, idproducto, idsuper, cantidad, precio)
+            VALUES('1','2','1','2', '2,49');
+insert into compraproductosuper(idcompra, idproducto, idsuper, cantidad, precio)
+            VALUES('1','3','1','1','1,22');
+insert into compraproductosuper(idcompra, idproducto, idsuper, cantidad, precio)
+            VALUES('1','8','1','1','3,45');
+insert into compraproductosuper(idcompra, idproducto, idsuper, cantidad, precio)
+            VALUES('1','6','2','1','1,65');
+insert into compraproductosuper(idcompra, idproducto, idsuper, cantidad, precio)
+            VALUES('2','8','1','1','3,45');
+insert into compraproductosuper(idcompra, idproducto, idsuper, cantidad, precio)
+            VALUES('2','6','2','1','1,65');
+
+
+-- Registrar comidas
+insert into comida(idusuario, idreceta) values('2','1');
+insert into comida(idusuario, idreceta) values('3','1');
+insert into comida(idusuario, idreceta) values('2','4');
+insert into comida(idusuario, idreceta) values('3','3');
+
+COMMIT;
--- tablas.sql
+++ tablas.sql
@@ -219,4 +219,32 @@ CREATE TABLE RecetaProducto(
 );
 
 
+-- Procedimientos y funciones
+CREATE OR REPLACE FUNCTION ObtenerGastoTotal(iduser IN number)
+   RETURN number
+IS
+   output number;
+   cursor c1 is
+   SELECT sum(c.Precio)
+     FROM CompraProductoSuper c, Compra com
+     WHERE com.IDUsuario = iduser and
+      com.IDCompra = c.IDCompra;
+BEGIN
+   open c1;
+   fetch c1 into output;
+   close c1;
+RETURN output;
+END;
+/
+
+CREATE OR REPLACE FUNCTION NumeroTotalComidas(iduser IN number)
+RETURN NUMBER IS output NUMBER;
+BEGIN
+ select count(*) INTO output
+ from Comida c
+ where c.IDUsuario = iduser;
+ return (output);
+END;
+/
+
 COMMIT;
